6072a326847a540ed5d82e9b01c8624bcc2933ce
bennett-wright <bennett.wright3@gmail.com>
2018-04-29 05:32:48 -0700

    Initial Commit
    Temperature Calculator using size classes

---
+++ TempCalc-size-classes/ViewController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+using UIKit;
+
+namespace TempCalcsizeclasses
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        private bool humid = false;
+        private void compute(object sender, EventArgs args)
+        {
+            if (humid)
+                ResultLabel.Text = String.Format("Result: {0} F",
+                    Math.Round(calculate.getHeatIndex(Double.Parse(FahrenheitField.Text),
+                        Double.Parse(HumidityField.Text) / 100)));
+
+            else
+                ResultLabel.Text = String.Format("Result: {0} F",
+                   Math.Round(calculate.getWindChill(Double.Parse(FahrenheitField.Text),
+                       (int)WindSlider.Value)));
+        }
+
+        partial void switchActionSheet(UISwitch sender)
+        {
+            string title = HumiditySwitch.On ? "Turn on Humidity?" : "Turn off Humidity";
+            var controller = UIAlertController.Create(title, null, UIAlertControllerStyle.ActionSheet);
+
+            var yesAction = UIAlertAction.Create("Yes, I'm Sure!", UIAlertActionStyle.Default,
+                (action) =>
+                {
+                    humid = HumidityField.Enabled = HumiditySwitch.On = HumiditySwitch.On;
+                });
+
+
+            var noAction = UIAlertAction.Create("No way!", UIAlertActionStyle.Cancel,
+                (action) =>
+                {
+                    humid = HumidityField.Enabled = HumiditySwitch.On = !HumiditySwitch.On;
+                });
+
+            controller.AddAction(yesAction);
+            controller.AddAction(noAction);
+            var ppc = controller.PopoverPresentationController;
+            if (ppc != null)
+            {
+                ppc.SourceView = sender;
+                ppc.SourceRect = sender.Bounds;
+            }
+
+            PresentViewController(controller, true, null);
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            //dismiss the keyboard on background touch
+            View.AddGestureRecognizer(new UITapGestureRecognizer(() =>
+            {
+                FahrenheitField.ResignFirstResponder();
+                HumidityField.ResignFirstResponder();
+            }));
+
+            HumidityField.EditingDidEnd += (sender, e) =>
+            {
+                if (HumidityField.Text == String.Empty)
+                    HumidityField.Text = "0";
+                compute(sender, e);
+            };
+
+            FahrenheitField.EditingDidEnd += (sender, e) =>
+            {
+                if (FahrenheitField.Text == String.Empty)
+                    FahrenheitField.Text = "0";
+                compute(sender, e);
+            };
+
+            //when slider value is changed, update UI
+            WindSlider.ValueChanged += (sender, e) =>
+            {
+                WindSpeedLabel.Text = String.Format("Wind Speed (0-100 mph): {0}",
+                                        (int)WindSlider.Value);
+
+                //make sure text isnt empty
+                if (FahrenheitField.Text == String.Empty)
+                    FahrenheitField.Text = "0";
+                compute(sender, e);
+            };
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ TempCalc-size-classes/calculate.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace TempCalcsizeclasses
+{
+    public class calculate
+    {
+        public calculate()
+        {
+        }
+
+        public static double getWindChill(double fDegrees, int windSpeed)
+        {
+            //temperature has to be below 50 degrees fahrenheit and
+            //wind speed has to be above 3mph before wind chill can
+            //take place
+            if (fDegrees < 50 && windSpeed > 3)
+                return 35.74 + (0.6215 * fDegrees) - 35.75 * Math.Pow(windSpeed, 0.16)
+                 + (0.4275 * fDegrees) * Math.Pow(windSpeed, 0.16);
+
+            else
+                return fDegrees;
+        }
+
+        public static double getHeatIndex(double fehrenheit, double humidity)
+        {
+            //equation:
+            //http://www.wpc.ncep.noaa.gov/html/heatindex_equation.shtml
+            double index = 0.5 * (fehrenheit + 61 + ((fehrenheit - 68) * 1.2))
+                         + (humidity * 0.094);
+
+            if (index < 80)
+                return index;
+
+            else
+                index = -42.379 + (2.04901523 * fehrenheit) + (10.14333127 * humidity)
+                         - (0.22475541 * fehrenheit * humidity) 
+                         - (6.83783 * Math.Pow(10, -3) * Math.Pow(fehrenheit, 2))
+                         - (5.481717 * Math.Pow(10, -2)  * Math.Pow(humidity, 2))
+                         + (1.22874 * Math.Pow(10, -3) * Math.Pow(fehrenheit, 2) * humidity)
+                         + (8.5282 * Math.Pow(10, -4) * fehrenheit * Math.Pow(humidity, 2))
+                         - (1.99 * Math.Pow(10, -6) * Math.Pow(fehrenheit, 2) * Math.Pow(humidity, 2));
+
+            //make adjustments based on temp and humidity
+            //humidity has to be less than 13% and temp is
+            //between 80 and 112
+            if(humidity < .13 && fehrenheit > 80 && fehrenheit < 112)
+            {
+                index -= ((13 - humidity) / 4)
+                    * Math.Sqrt((17 - Math.Abs(fehrenheit - 95)) / 17);
+            }
+
+
+            else if(humidity > .85 && fehrenheit > 80 && fehrenheit < 87)
+                index += (((humidity - 85) / 10) * (87 - fehrenheit) / 5);
+            
+
+            return index;
+        }
+    }
+}
